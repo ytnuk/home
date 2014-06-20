11394c9cfdc24053c05ef6dbc7ce4eeca83f2b57
Vít Kutný <vitkutny@outlook.com>
2014-06-20 23:22:57 +0200

    dev

---
+++ src/Home/Admin/Presenter.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WebEdit\Home\Admin;
+
+use WebEdit\Admin;
+
+final class Presenter extends Admin\Presenter {
+    
+}
---
+++ src/Home/Presenter.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WebEdit\Home;
+
+use WebEdit\Front;
+
+final class Presenter extends Front\Presenter {
+    
+}
