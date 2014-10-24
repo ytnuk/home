1c7a388fc3f6b5db8914acc856186a775f0e92d5
Vít Kutný <vitkutny@outlook.com>
2014-10-24 09:28:51 +0200

    dev

--- src/Home/Extension.php
+++ src/Home/Extension.php
@@ -9,20 +9,22 @@ use WebEdit\Translation;
 final class Extension extends Module\Extension implements Routing\Provider, Translation\Provider
 {
 
+    public function getResources()
+    {
+        return [
+            'locale' => 'en',
+        ];
+    }
+
     public function getRoutingResources()
     {
         return [
             'routes' => [
                 '[<locale [a-z]{2}(_[A-Z]{2})?>/]<module>[/<action>][/<id [0-9]+>]' => [
-                    'module' => [
-                        Routing\Route::VALUE => 'Home:Front',
-                        Routing\Route::TRANSLATE => TRUE,
-                    ],
+                    'module' => 'Home:Front',
                     'presenter' => 'Presenter',
-                    'action' => [
-                        Routing\Route::VALUE => 'view',
-                        Routing\Route::TRANSLATE => TRUE,
-                    ],
+                    'action' => 'view',
+                    'locale' => $this['locale']
                 ]
             ]
         ];
