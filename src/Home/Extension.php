66ca50b7d147a4c4c9c46c045611555d26aaef38
Vít Kutný <vitkutny@outlook.com>
2014-10-29 01:00:29 +0100

    refactor

--- src/Home/Extension.php
+++ src/Home/Extension.php
@@ -11,23 +11,11 @@ final class Extension extends Module\Extension implements Routing\Provider, Tran
 
 	public function getResources()
 	{
-		return [
-			'locale' => 'en',
-		];
+		return ['locale' => 'en',];
 	}
 
 	public function getRoutingResources()
 	{
-		return [
-			'routes' => [
-				'[<locale [a-z]{2}(_[A-Z]{2})?>/]<module>[/<action>][/<id [0-9]+>]' => [
-					'module' => 'Home:Front',
-					'presenter' => 'Presenter',
-					'action' => 'view',
-					'locale' => $this['locale']
-				]
-			]
-		];
+		return ['routes' => ['[<locale [a-z]{2}(_[A-Z]{2})?>/]<module>[/<action>][/<id [0-9]+>]' => ['module' => 'Home:Front', 'presenter' => 'Presenter', 'action' => 'view', 'locale' => $this['locale']]]];
 	}
-
 }
